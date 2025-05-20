4c4f962098752afd6687df864837a4ad1e334db1
권단비 <dban@yonsei.ac.kr>
2025-05-20 14:35:37 +0900

    Hide axes ticks and add legend

--- stream_run.py
+++ stream_run.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 
 from seat import plot_seats_with_pairs, csp_points
 
-
 # 30초 자동 새로고침
 st_autorefresh(interval=30000, key="datarefresh")
 
 st.title("A507 실시간 좌석 및 공기질 모니터링")
 
 # 재실 좌석을 코드에 직접 정의 (필요 시 수정)
-occupied = {"1", "2", "4", "6", "7"}  # 예시: 변경할 때 여기만 지우고 다시 배포
+occupied = {"1", "2", "4", "6", "7"}
 
 # 플롯 생성
 fig, ax = plt.subplots(figsize=(6, 5))
